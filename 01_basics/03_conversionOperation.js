b36f632f5f92e268782e11202dbcaa01434dd1d4
Codemonk <78207195+vickyraut@users.noreply.github.com>
2023-12-18 10:03:30 +0000

    Day 1 done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -6,9 +6,42 @@ let valNumber = Number(score)
 console.log(typeof valNumber); // Number
 
 score = "33abc"
-console.log(typeof score); //NaN
+valNumber = Number(score)
+console.log(valNumber); //NaN
+console.log(typeof valNumber); // number 
 
 // "33" => 33
 // "33abc" => NaN
 // true => 1; false = 0
 // "" => false; "vicky" => true
+
+// ******************************OPERATIONS*************************************
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "Vicky"
+let str2 = "Raut"
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
+
+// Sonme confusing opearations
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// "===" is used to strictly check that both variables are equal or Not means it check datatype and value both
+
+console.log("2" == 2); // true
+console.log("2" === 2); // false
+
---
+++ 01_basics/04_datatypes-summary.js
@@ -0,0 +1,25 @@
+// Primitive
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. null 
+// 5. undefined
+// 6. Symbols
+// 7. BigInt
+
+// Symbol gives unique id
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// Non-Primitives
+// 1. Array
+// 2. Object
+// 3. Array
+
+/* 
+    Dynamically-typed languages are those (like JavaScript) where the interpreter 
+    assigns variables a type at runtime based on the variable's value at the time.
+*/
+
